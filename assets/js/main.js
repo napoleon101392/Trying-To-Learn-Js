ce1d77b5541066dbf8deea3052f397f0927f9cbb
Napoleon Carino Jr <napoleoncarino@Napoleon.connectify>
2018-07-05 22:35:16 +0800

    cleaning

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,105 +5,94 @@ const Tasks = {
 
   addTodo(task) {
     this.todo.push(task);
-    todoList();
+    Template.todo();
   },
+
   removeTodo(index) {
     this.todo.splice(index, 1);
-    todoList();
+    Template.todo();
   },
 
   addDone(task) {
     this.done.push(task);
-    todoList();
-    doneList();
+    Template.todo();
+    Template.done();
   },
+
   removeDone(index) {
     this.done.splice(index, 1);
-    todoList();
-    doneList();
+    Template.todo();
+    Template.done();
   },
 
   closeTask(task) {
     this.close.push(task);
-    todoList();
-    doneList();
-    closeList();
+    Template.todo();
+    Template.done();
+    Template.close();
   }
 }
 
-function todoList()
-{
-  $("#todo .body").html(``);
+const Template = {
+  todo() {
+    $("#todo .body").html(``);
 
-  for(var i = 0; i < Tasks.todo.length; i++) {
-    var template = `
-      <div>
-        <p>${Tasks.todo[i]}</p>
-        <button id="done-${i}" onclick="done(this)" value="${Tasks.todo[i]}">DONE</button>
-        <button onclick="deleteTodo(this)">DELETE</button>
-      </div>`;
+    for(var i = 0; i < Tasks.todo.length; i++) {
+      var template = `
+        <div>
+          <p>${Tasks.todo[i]}</p>
+          <button id="${i}" onclick="done(this)" value="${Tasks.todo[i]}">DONE</button>
+          <button onclick="Tasks.removeTodo(${this.id});">DELETE</button>
+        </div>`;
 
-    $("#todo .body").append(template);
+      $("#todo .body").append(template);
+    }
+  },
+
+  done() {
+    $("#done .body").html(``);
+
+    for(var i = 0; i < Tasks.done.length; i++) {
+      var template = `
+        <div>
+          <p>${Tasks.done[i]}</p>
+          <button id="${i}" onclick="undo(this)" value="${Tasks.done[i]}">Undo</button>
+          <button id="${i}" onclick="closeDone(this)" value="${Tasks.done[i]}">Close</button>
+        </div>`;
+
+      $("#done .body").append(template);
+    }
+  },
+
+  close() {
+    $("#close .body").html(``);
+
+    for(var i = 0; i < Tasks.close.length; i++) {
+      var template = `
+        <div>
+          <p>${Tasks.close[i]}</p>
+        </div>`;
+
+      $("#close .body").append(template);
+    }
   }
 }
 
-function doneList()
-{
-  $("#done .body").html(``);
-
-  for(var i = 0; i < Tasks.done.length; i++) {
-    var template = `
-      <div>
-        <p>${Tasks.done[i]}</p>
-        <button id="undo-${i}" onclick="undo(this)" value="${Tasks.done[i]}">Undo</button>
-        <button id="close-${i}" onclick="closeDone(this)" value="${Tasks.done[i]}">Close</button>
-      </div>`;
-
-    $("#done .body").append(template);
-  }
-}
-
-function closeList()
-{
-  $("#close .body").html(``);
-
-  for(var i = 0; i < Tasks.close.length; i++) {
-    var template = `
-      <div>
-        <p>${Tasks.close[i]}</p>
-      </div>`;
-
-    $("#close .body").append(template);
-  }
-}
 
 function done(attr) {
-  index = attr.id.slice(5);
-
-  Tasks.removeTodo(index);
+  Tasks.removeTodo(attr.id);
 
   Tasks.addDone(attr.value);
 }
 
-function deleteTodo(attr) {
-  index = attr.id.slice(5);
-
-  Tasks.removeTodo(index);
-}
-
 function undo(attr) {
-  index = attr.id.slice(6);
-
   Tasks.addTodo(attr.value);
 
-  Tasks.removeDone(index);
+  Tasks.removeDone(attr.id);
 }
 
 function closeDone(attr) {
-  // remove done
-  index = attr.id.slice(6);
-
-  Tasks.removeDone(index);
+  Tasks.removeDone(attr.id);
 
   Tasks.closeTask(attr.value)
 }
